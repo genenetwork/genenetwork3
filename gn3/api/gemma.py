9853de2856214ddaa1ab84f8134aaf4e61d1e9c1
BonfaceKilz <me@bonfacemunyoki.com>
2021-02-17 00:38:18 +0300

    Add stubbed gemma endpoints

--- gn3/api/gemma.py
+++ gn3/api/gemma.py
@@ -9,3 +9,28 @@ gemma = Blueprint("gemma", __name__)
 def index() -> str:
     """Test endpoint"""
     return jsonify(result="hello world")
+
+
+@gemma.route("/version")
+def get_version():
+    pass
+
+
+@gemma.route("/k-compute/<token>", methods=["POST"])
+def run_k_compute(token) -> str:
+    pass
+
+
+@gemma.route("/lmm/gwa/<token>/<k_file_name>", methods=["POST"])
+def run_gwa(token, k_file_name) -> str:
+    pass
+
+
+@gemma.route("/pheno-permutation/<token>", methods=["POST"])
+def run_pheno_permutation(token) -> str:
+    pass
+
+
+@gemma.route("/lmm2/loco/<token>", methods=["POST"])
+def run_gemma_with_loco(token) -> str:
+    pass
