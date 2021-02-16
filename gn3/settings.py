024fe45062da1987767a4de7d909ec995598bf8f
BonfaceKilz <me@bonfacemunyoki.com>
2021-02-16 22:20:28 +0300

    Add extra default config for bcrypt

--- gn3/settings.py
+++ gn3/settings.py
@@ -4,6 +4,7 @@ import tempfile
 import os
 
 APP_DEFAULTS = {
+    "BCRYPT_SALT": "$2b$12$mxLvu9XRLlIaaSeDxt8Sle",  # Change this!
     "GEMMA_WRAPPER_CMD": os.environ.get("GEMMA_WRAPPER", "gemma-wrapper"),
     "TMPDIR": os.environ.get("TMPDIR", tempfile.gettempdir()),
     "REDIS_URI": "redis://localhost:6379/0"
