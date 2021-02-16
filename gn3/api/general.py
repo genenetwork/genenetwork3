cef84aa5509ffc9b0f744c88586a734cec62d446
BonfaceKilz <me@bonfacemunyoki.com>
2021-02-16 23:58:22 +0300

    Return the correct message and status code in "/metadata/upload"

--- gn3/api/general.py
+++ gn3/api/general.py
@@ -11,12 +11,20 @@ from gn3.file_utils import extract_uploaded_file
 general = Blueprint("general", __name__)
 
 
-@general.route("/metadata/upload")
+@general.route("/metadata/upload", methods=["POST"])
 def upload_metadata():
     """Extract uploaded file to gn3 temporary directory; and if successful return
 a TOKEN to the user
 
     """
-    results = extract_uploaded_file(gzipped_file=request.files["file"],
-                                    target_dir=current_app.get("TMPDIR"))
-    return jsonify(results)
+    file_ = request.files.get("file")
+    if not file_:
+        return jsonify(status=128, error="Please provide a file!"), 400
+
+    status = 201
+    results = extract_uploaded_file(
+        gzipped_file=request.files["file"],
+        target_dir=current_app.config["APP_DEFAULTS"].get("TMPDIR"))
+    if results.get("status") > 0:
+        status = 500
+    return jsonify(results), status
