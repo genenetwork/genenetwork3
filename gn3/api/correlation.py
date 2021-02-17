38f35b7cfd14bc6cb33ebd2e615e30c479dea60f
Alexander Kabui <alexanderkabua@gmail.com>
2021-02-18 00:11:31 +0300

    add function  for getting page data

--- gn3/api/correlation.py
+++ gn3/api/correlation.py
@@ -5,6 +5,111 @@ from flask import jsonify
 correlation = Blueprint("correlation", __name__)
 
 
+@app.route("/loading", methods=('POST',))
+def loading_page():
+    logger.info(request.url)
+    initial_start_vars = request.form
+    start_vars_container = {}
+    n_samples = 0  # ZS: So it can be displayed on loading page
+    if 'wanted_inputs' in initial_start_vars:
+        wanted = initial_start_vars['wanted_inputs'].split(",")
+        start_vars = {}
+        for key, value in list(initial_start_vars.items()):
+            if key in wanted:
+                start_vars[key] = value
+
+        if 'n_samples' in start_vars:
+            n_samples = int(start_vars['n_samples'])
+        else:
+            sample_vals_dict = json.loads(start_vars['sample_vals'])
+            if 'group' in start_vars:
+                dataset = create_dataset(
+                    start_vars['dataset'], group_name=start_vars['group'])
+            else:
+                dataset = create_dataset(start_vars['dataset'])
+            genofile_samplelist = []
+            samples = start_vars['primary_samples'].split(",")
+            if 'genofile' in start_vars:
+                if start_vars['genofile'] != "":
+                    genofile_string = start_vars['genofile']
+                    dataset.group.genofile = genofile_string.split(":")[0]
+                    genofile_samples = run_mapping.get_genofile_samplelist(
+                        dataset)
+                    if len(genofile_samples) > 1:
+                        samples = genofile_samples
+
+            for sample in samples:
+                if sample in sample_vals_dict:
+                    if sample_vals_dict[sample] != "x":
+                        n_samples += 1
+
+        start_vars['n_samples'] = n_samples
+        start_vars['wanted_inputs'] = initial_start_vars['wanted_inputs']
+
+        start_vars_container['start_vars'] = start_vars
+    else:
+        start_vars_container['start_vars'] = initial_start_vars
+
+    rendered_template = render_template("loading.html", **start_vars_container)
+
+    return rendered_template
+
+
+def get_loading_page_data(initial_start_vars, create_dataset, get_genofile_samplelist):
+
+    # not sure how to test this  should expect
+    # convert  this function no need to submit form twice
+
+    start_vars_container = {}
+    n_samples = 0
+    if "wanted_inputs" in initial_start_vars:
+        wanted = initial_start_vars["wanted"].split(",")
+        start_vars = {}
+
+        for key, value in initial_start_vars.items():
+            if key in wanted:
+                start_vars[key] = value
+
+        if "n_samples" in start_vars:
+            n_samples = int(start_vars["n_samples"])
+
+        else:
+            sample_vals_dict = json.loads(start_vars['sample_vals'])
+
+            dataset = create_dataset(start_vars['dataset'], group_name=start_vars['group']
+                                     ) if "group" in start_vars else create_dataset(start_vars['dataset'])
+
+            genofile_samplelist = []
+
+            samples = start_vars['primary_samples'].split(",")
+
+            if 'genofile' in start_vars:
+                genofile_string = start_vars['genofile']
+                dataset.group.genofile = genofile_string.split(":")[0]
+
+                genofile_samples = get_genofile_samplelist(
+                    dataset)
+
+                if len(genofile_samples) > 1:
+                    samples = genofile_samples
+
+            for sample in samples:
+                if sample in sample_vals_dict:
+                    if sample_vals_dict[sample] != "x":
+                        n_samples += 1
+
+        start_vars['n_samples'] = n_samples
+        start_vars['wanted_inputs'] = initial_start_vars['wanted_inputs']
+
+        start_vars_container['start_vars'] = start_vars
+
+    else:
+
+        start_vars_container['start_vars'] = initial_start_vars
+
+    return start_vars_container
+
+
 @correlation.route("/")
 def index() -> str:
     """Test endpoint"""
