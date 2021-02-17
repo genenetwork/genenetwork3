234864dd171c1a1c42da3ac9cca40dd65984081a
BonfaceKilz <me@bonfacemunyoki.com>
2021-02-17 17:53:12 +0300

    Add GENODIR extra param

--- gn3/settings.py
+++ gn3/settings.py
@@ -7,5 +7,6 @@ APP_DEFAULTS = {
     "BCRYPT_SALT": "$2b$12$mxLvu9XRLlIaaSeDxt8Sle",  # Change this!
     "GEMMA_WRAPPER_CMD": os.environ.get("GEMMA_WRAPPER", "gemma-wrapper"),
     "TMPDIR": os.environ.get("TMPDIR", tempfile.gettempdir()),
+    "GENODIR": "",
     "REDIS_URI": "redis://localhost:6379/0"
 }
