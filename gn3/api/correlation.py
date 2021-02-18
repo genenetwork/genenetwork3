e4c7a49dc9549e00fd388474aa442f9247c69668
Alexander Kabui <alexanderkabua@gmail.com>
2021-02-18 09:52:32 +0300

    modify code for correlation

--- gn3/api/correlation.py
+++ gn3/api/correlation.py
@@ -60,6 +60,23 @@ def get_loading_page_data(initial_start_vars, create_dataset, get_genofile_sampl
     return start_vars_container
 
 
+@app.route("/corr_compute", methods=["POST"])
+def corr_compute_page():
+
+    start_vars_container = get_loading_page_data(initial_start_vars=None,create_dataset=None,get_loading_page_data=None)
+
+    corr_object = show_corr_results.CorrelationResults(
+        start_vars=start_vars_container)
+
+    corr_results = corr_object.__dict__
+    # possibility of file being so large cause of the not sure whether to return a file
+    # initial setup for return type
+
+    return jsonify({"corr_results":{
+        "dataset":"dataset"
+        }})
+
+
 @correlation.route("/")
 def index() -> str:
     """Test endpoint"""
