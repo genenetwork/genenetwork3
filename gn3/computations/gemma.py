1f05d9631427b5f44bfccd70d19301f166c23adf
BonfaceKilz <me@bonfacemunyoki.com>
2021-02-19 17:08:45 +0300

    Append random 6 chars to generated phenotype file

--- gn3/computations/gemma.py
+++ gn3/computations/gemma.py
@@ -1,10 +1,19 @@
 """Procedures related gemma computations"""
+import random
+import string
+
+
+def generate_random_n_string(n_length):
+    """Generate a random string that is N chars long"""
+    return ''.join(random.choice(string.ascii_uppercase + string.digits)
+                   for _ in range(n_length))
 
 
 def generate_pheno_txt_file(trait_filename: str,
                             values: str,
                             tmpdir: str = "/tmp") -> str:
     """Given VALUES, and TMPDIR, generate a valide traits file"""
+    trait_filename += f"_{generate_random_n_string(6)}"
     with open(f"{tmpdir}/gn2/{trait_filename}", "w") as _file:
         for value in values:
             if value == "x":
