4fd32ff4cc12fcde146c9e4095c1da3e7cd2dd85
Alexander Kabui <alexanderkabua@gmail.com>
2021-02-20 11:09:57 +0300

    test loading correlation data using api endpoint

--- gn3/api/correlation.py
+++ gn3/api/correlation.py
@@ -1,31 +1,36 @@
 """Endpoints for computing correlation"""
 from flask import Blueprint
 from flask import jsonify
+from flask import request
 
+from gn3.correlation.correlation_computations import get_loading_page_data
+from gn3.correlation.correlation_computations import get_loading_page_data
 from gn3.correlation.correlation_computations import get_loading_page_data
 from gn3.correlation.show_corr_results import CorrelationResults
 
 correlation = Blueprint("correlation", __name__)
 
 
-@correlation.route("/corr_compute")
+@correlation.route("/corr_compute",methods=["POST"])
 def corr_compute_page():
     """api for doing the actual correlation"""
 
-    start_vars_container = get_loading_page_data(initial_start_vars=None,
-                                                 create_dataset=None, get_genofile_samplelist=None)
 
-    corr_object = CorrelationResults(
-        start_vars=start_vars_container)
+    initial_start_vars = request.json
 
-    corr_results = corr_object.__dict__
+    start_vars_container = get_loading_page_data(initial_start_vars=initial_start_vars)
+
+    # corr_object = CorrelationResults(
+    #     start_vars=start_vars_container)
+
+    # corr_results = corr_object.__dict__
+
+
     # print(corr_results)
     # possibility of file being so large cause of the not sure whether to return a file
     # initial setup for return type
 
-    return jsonify({"corr_results": {
-        "dataset": corr_results
-    }})
+    return jsonify(start_vars_container)
 
 
 @correlation.route("/")
