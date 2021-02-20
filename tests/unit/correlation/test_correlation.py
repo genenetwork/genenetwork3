a0812df3737d019177b096f5da9c4e666b358669
Alexander Kabui <alexanderkabua@gmail.com>
2021-02-20 08:56:20 +0300

    add tests for correlation

--- tests/unit/correlation/test_correlation.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from gn3.api.correlation import get_loading_page_data
-
-class TestCorrelationUtility(unittest.TestCase):
-
-
-	def test_loading_page_data():
-		"""test for loading page data"""
-		pass
