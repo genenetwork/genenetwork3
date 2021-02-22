a3337ffdee2570ddd154428ed38312aeee888960
Alexander Kabui <alexanderkabua@gmail.com>
2021-02-22 18:08:50 +0300

    pep8 formatting for module

--- tests/integration/test_correlation.py
+++ tests/integration/test_correlation.py
@@ -13,10 +13,14 @@ class CorrelationAPITest(unittest.TestCase):
 
     def test_corr_compute(self):
         """Test that the correct response in correlation"""
+
+        sample_vals = """{"C57BL/6J":"7.197","DBA/2J":"7.148","B6D2F1":"6.999"}"""
         post_data = {
             "corr_return_results": 100,
             "corr_sample_method": "pearson",
             "corr_type": "sample",
+            "primary_samples": "C57BL/6J,DBA/2J,B6D2F1",
+            "sample_vals": sample_vals,
             "dataset": "Temp",
             "group": "G1",
             "corr_dataset": "D1",
@@ -26,16 +30,20 @@ class CorrelationAPITest(unittest.TestCase):
             "p_range_lower": "-1.00",
             "p_range_upper": "1.00",
             "trait_id": "1444666_at",
-            "wanted_inputs": "p_range_lower,p_range_upper,min_expr,corr_samples_group,corr_dataset,n_samples,corr_type,corr_sample_method,corr_return_results,trait_id,group,dataset"
+            "wanted_inputs": "primary_samples,sample_vals,p_range_lower,p_range_upper,min_expr,corr_samples_group,corr_dataset,n_samples,corr_type,corr_sample_method,corr_return_results,trait_id,group,dataset"
         }
 
         expected_data_correlation = {
             "corr_method": "pearson",
+            'formatted_corr_type': "Genetic Correlation (Pearson's r)",
             "corr_type": "sample",
             "dataset": {
                 "group": {
 
-                    "genofile": ""
+                    "genofile": "",
+                    "samplelist": "S1",
+                    "parlist": "",
+                    "f1list": ""
 
                 }
             },
@@ -85,7 +93,5 @@ class CorrelationAPITest(unittest.TestCase):
         response = self.app.post(
             "/corr_compute", json=post_data, follow_redirects=True)
         # self.assertEqual(response.get_json().get("result"), "hello world")
-
-        print(response.get_json())
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.get_json(), expected_data_correlation)
