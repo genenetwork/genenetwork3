d204447a5e84f9145ce45b43b43923ac21709b0f
Alexander Kabui <alexanderkabua@gmail.com>
2021-02-22 18:00:37 +0300

    refactor endpoint for /corr_compute

--- gn3/api/correlation.py
+++ gn3/api/correlation.py
@@ -5,8 +5,7 @@ from flask import jsonify
 from flask import request
 
 from gn3.correlation.correlation_computations import get_loading_page_data
-from gn3.correlation.correlation_computations import get_loading_page_data
-from gn3.correlation.correlation_computations import get_loading_page_data
+from gn3.correlation.correlation_computations import compute_correlation
 from gn3.correlation.show_corr_results import CorrelationResults
 
 correlation = Blueprint("correlation", __name__)
@@ -14,22 +13,11 @@ correlation = Blueprint("correlation", __name__)
 
 @correlation.route("/corr_compute", methods=["POST"])
 def corr_compute_page():
-    """api for doing the actual correlation"""
+    """api for doing  correlation"""
 
     initial_start_vars = request.json
-    start_vars_container = get_loading_page_data(
-        initial_start_vars=initial_start_vars)
 
-    start_vars = start_vars_container["start_vars"]
-
-    corr_object = CorrelationResults(
-        start_vars=start_vars)
-
-    # return cor
-
-    corr_results = corr_object.do_correlation(start_vars=start_vars)
-    # possibility of file being so large cause of the not sure whether to return a file
-
+    corr_results = compute_correlation(init_start_vars=initial_start_vars)
     try:
         corr_results = json.loads(json.dumps(
             corr_results, default=lambda o: o.__dict__))
@@ -37,9 +25,3 @@ def corr_compute_page():
         return corr_results
     except Exception as e:
         return jsonify({"error": str(e)})
-
-
-@correlation.route("/")
-def index() -> str:
-    """Test endpoint"""
-    return jsonify(result="hello world")
