67329e17736b489ec43b9052d1ff5a05addc1346
Alexander Kabui <alexanderkabua@gmail.com>
2021-03-05 16:25:44 +0300

    add tests for show_corr_results

--- tests/unit/correlation/test_show_corr_results.py
+++ tests/unit/correlation/test_show_corr_results.py
@@ -5,6 +5,7 @@ import json
 import os
 from gn3.correlation.show_corr_results import CorrelationResults
 
+
 def file_path(relative_path):
     # adopted from github
     dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,34 +14,36 @@ def file_path(relative_path):
     return new_path
 
 
-def create_trait(dataset="Temp",name=None,cellid=None):
-    return "trait results "
+def create_trait(dataset="Temp", name=None, cellid=None):
+    return "trait results"
 
-def create_dataset(dataset_name="Temp",dataset_type="Temp",group_name=None):
+
+def create_dataset(dataset_name="Temp", dataset_type="Temp", group_name=None):
     return "dataset results"
 
-def get_species_dataset_trait(self,start_vars):
 
+def get_species_dataset_trait(self, start_vars):
     """
     how this function works is that it sets the self.dataset and self.species and self.this_trait
     """
-    self.create_trait="trait results"
-    self.create_dataset  = "dataset results"
+
+    self.species = "this species data"
+    self.dataset = "dataset results"
     self.this_trait = "this trait has been set"
 
 
 class TestCorrelationResults(unittest.TestCase):
 
-
     def setUp(self):
 
         with open(file_path("./correlation_test_data.json")) as json_file:
-            self.correlation_data = json.load(json_file) 
+            self.correlation_data = json.load(json_file)
 
     def tearDown(self):
 
         self.correlation_data = ""
         # pass
+
     def test_for_assertion(self):
         pass
         with self.assertRaises(AssertionError):
@@ -56,12 +59,39 @@ class TestCorrelationResults(unittest.TestCase):
             "corr_return_results": 100
         }
 
-        corr_results_object = CorrelationResults(start_vars=self.correlation_data)
+        corr_results_object = CorrelationResults(
+            start_vars=self.correlation_data)
 
-        corr_results_object.refactored_do_correlation(start_vars=self.correlation_data,create_dataset=create_dataset,create_trait=create_trait,get_species_dataset_trait=get_species_dataset_trait)
+        corr_results = corr_results_object.refactored_do_correlation(
+            start_vars=self.correlation_data, create_dataset=create_dataset, create_trait=create_trait, get_species_dataset_trait=get_species_dataset_trait)
+
+        # assert for self.corr_results group
+        # mock data should use more reasonable results
+
+        self.assertEqual(corr_results.this_trait, "this trait has been set")
+        self.assertEqual(corr_results.species, "this species data")
+        self.assertEqual(corr_results.dataset, "dataset results")
+
+        # test using where type  is temp
+
+        self.correlation_data["dataset"] = "Temp"
+
+        self.correlation_data["group"] = "G1"
+
+        corr_results_object_with_temp = CorrelationResults(
+            start_vars=self.correlation_data)
+        corr_results = corr_results_object.refactored_do_correlation(
+            start_vars=self.correlation_data, create_dataset=create_dataset, create_trait=create_trait, get_species_dataset_trait=get_species_dataset_trait)
+
+        # asssert where the dataset is temp
+
+        self.assertEqual(corr_results.this_trait, "trait results")
+
+        self.assertEqual(corr_results.dataset,"dataset results")
+
     def test_for_creating_traits_and_dataset(self):
         """and dummy tests for creating trait and dataset with dataset=Temp"""
-        self.assertEqual(2,2)
+        self.assertEqual(2, 2)
         return
 
         start_vars = {
@@ -82,8 +112,6 @@ class TestCorrelationResults(unittest.TestCase):
 
         corr_object = CorrelationResults(start_vars=start_vars)
         results = corr_object.do_correlation(start_vars=start_vars)
-
-
 
     def test_convert_to_mouse_gene_id(self):
         """test for converting mouse to gene id"""
