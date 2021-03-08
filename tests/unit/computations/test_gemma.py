b24f3e646f164fba75d45af56ab3412be3ac60c3
BonfaceKilz <me@bonfacemunyoki.com>
2021-03-08 15:26:33 +0300

    Delete "generate_gemma_computation_cmd"

--- tests/unit/computations/test_gemma.py
+++ tests/unit/computations/test_gemma.py
@@ -5,7 +5,6 @@ from unittest import mock
 from gn3.computations.gemma import generate_gemma_cmd
 from gn3.computations.gemma import generate_hash_of_string
 from gn3.computations.gemma import generate_pheno_txt_file
-from gn3.computations.gemma import generate_gemma_computation_cmd
 
 
 class TestGemma(unittest.TestCase):
@@ -31,26 +30,6 @@ class TestGemma(unittest.TestCase):
         """Test that a string is hashed correctly"""
         self.assertEqual(generate_hash_of_string("I^iQP&TlSR^z"),
                          "hMVRw8kbEp49rOmoIkhMjA")
-
-    @mock.patch("gn3.computations.gemma.do_paths_exist")
-    def test_compose_k_computation_cmd(self, mock_pathsp):
-        """Test that a K computation cmd is constructed properly"""
-        mock_pathsp.return_value = True
-        self.assertEqual(
-            generate_gemma_computation_cmd(
-                gemma_cmd="gemma-wrapper",
-                gemma_wrapper_kwargs=None,
-                gemma_kwargs={
-                    "geno_filename": "genofile.txt",
-                    "trait_filename": "test.txt",
-                    "covar_filename": "genofile_snps.txt"
-                },
-                output_file="/tmp/gn2/k_output_gUFhGu4rLG7k+CXLPk1OUg.txt",
-            ), ("gemma-wrapper --json -- "
-                "-g genofile.txt -p "
-                "test.txt -a genofile_snps.txt "
-                "-gk > /tmp/gn2/"
-                "k_output_gUFhGu4rLG7k+CXLPk1OUg.txt"))
 
     @mock.patch("gn3.computations.gemma.get_hash_of_files")
     def test_compute_k_values_without_loco(self, mock_get_hash):
