27fbd524f6cc8ce085ec0d31a3fb84a49136cb5f
Alexander Kabui <alexanderkabua@gmail.com>
2021-03-09 22:49:39 +0300

    pep8 formatting for show_corr_results

--- tests/unit/correlation/test_show_corr_results.py
+++ tests/unit/correlation/test_show_corr_results.py
@@ -3,62 +3,82 @@
 import unittest
 import json
 import os
-from gn3.correlation.show_corr_results import CorrelationResults
-from gn3.correlation.correlation_utility import AttributeSetter
 from unittest import mock
+from types import SimpleNamespace
+from gn3.correlation.show_corr_results import CorrelationResults
+# pylint: disable=unused-argument
+
+
 class MockGroup:
+    """mock  class for Group"""
+
     def __init__(self):
         self.samplelist = "add a mock for this"
         self.parlist = None
 
         self.filist = None
 
+    def __str__(self):
+        return self.__class__.__name__
+
+    def get_dict(self):
+        """get obj dict"""
+        return self.__dict__
+
 
 class MockCreateTrait:
+    """mock class for create trait"""
+
     def __init__(self):
         pass
 
     def get_dict(self):
-        raise NotImplementedError()
+        """class for getting dict items"""
+        return self.__dict__
 
     def __str__(self):
         return self.__class__.__name__
 
 
 class MockCreateDataset:
+    """mock class for create dataset"""
+
     def __init__(self):
 
         self.group = MockGroup()
 
     def get_trait_data(self, sample_keys):
+        """method for getting trait data"""
         raise NotImplementedError()
 
-    def retrieve_genes(symbol):
+    def retrieve_genes(self, symbol):
+        """method for retrieving genes"""
         raise NotImplementedError()
 
 
 def file_path(relative_path):
+    """getting abs path for file """
     # adopted from github
-    dir = os.path.dirname(os.path.abspath(__file__))
+    dir_name = os.path.dirname(os.path.abspath(__file__))
     split_path = relative_path.split("/")
-    new_path = os.path.join(dir, *split_path)
+    new_path = os.path.join(dir_name, *split_path)
     return new_path
 
 
 def create_trait(dataset="Temp", name=None, cellid=None):
+    """mock function for creating trait"""
     return "trait results"
 
 
 def create_dataset(dataset_name="Temp", dataset_type="Temp", group_name=None):
+    """mock  function to create dataset """
     return "dataset results"
 
 
-def get_species_dataset_trait(self, start_vars):
+def get_species(self, start_vars):
     """
     how this function works is that it sets the self.dataset and self.species and self.this_trait
     """
-    from types import SimpleNamespace
-
 
     with open(file_path("./dataset.json")) as dataset_file:
         results = json.load(dataset_file)
@@ -67,11 +87,11 @@ def get_species_dataset_trait(self, start_vars):
     with open(file_path("./group_data_test.json")) as group_file:
         results = json.load(group_file)
         self.group = SimpleNamespace(**results)
-    
 
-    self.dataset.group =  self.group
+    self.dataset.group = self.group
 
-    trait_dict = {'name': '1434568_at', 'dataset': self.dataset,'cellid': None, 'identification': 'un-named trait', 'haveinfo': True, 'sequence': None}
+    trait_dict = {'name': '1434568_at', 'dataset': self.dataset, 'cellid': None,
+                  'identification': 'un-named trait', 'haveinfo': True, 'sequence': None}
 
     trait_obj = SimpleNamespace(**trait_dict)
 
@@ -81,6 +101,7 @@ def get_species_dataset_trait(self, start_vars):
 
 
 class TestCorrelationResults(unittest.TestCase):
+    """unittests for Correlation Results"""
 
     def setUp(self):
 
@@ -94,21 +115,19 @@ class TestCorrelationResults(unittest.TestCase):
     def test_for_assertion(self):
         """test for assertion failures"""
         with self.assertRaises(AssertionError):
-            corr_results_object = CorrelationResults(start_vars={})
+            _corr_results_object = CorrelationResults(start_vars={})
 
-    
     @mock.patch("gn3.correlation.show_corr_results.CorrelationResults.process_samples")
-    def test_do_correlation(self,process_samples):
+    def test_do_correlation(self, process_samples):
         """test for doing correlation"""
         process_samples.return_value = None
         corr_object = CorrelationResults(start_vars=self.correlation_data)
 
+        with self.assertRaises(Exception) as _error:
 
-        with self.assertRaises(Exception) as error:
+            # xtodo;to be completed
 
-            #todo  to be completed
-
-
-
-            corr_results = corr_object.do_correlation(start_vars=self.correlation_data,create_dataset=create_dataset,
-                create_trait=None,get_species_dataset_trait=get_species_dataset_trait)
+            _corr_results = corr_object.do_correlation(start_vars=self.correlation_data,
+                                                       create_dataset=create_dataset,
+                                                       create_trait=None,
+                                                       get_species_dataset_trait=get_species)
