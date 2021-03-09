1b7950432ee589d5cb20d9bde7dd924f39550e45
Alexander Kabui <alexanderkabua@gmail.com>
2021-03-09 14:48:54 +0300

    remove mock functions in correlation_utility module

--- gn3/correlation/correlation_utility.py
+++ gn3/correlation/correlation_utility.py
@@ -2,46 +2,22 @@
 
 
 class AttributeSetter:
+    """class for setting Attributes"""
+
     def __init__(self, trait_obj):
         for key, value in trait_obj.items():
             setattr(self, key, value)
 
+    def __str__(self):
+        return self.__class__.__name__
 
-def create_dataset(dataset_name, dataset_type, group_name):
-    """mock function for creating dataset"""
-
-    dataset = AttributeSetter({
-        "group": AttributeSetter({
-            "genofile": "",
-            "samplelist": "S1",
-            "parlist": "",
-            "f1list": ""
-
-
-        })
-    })
-
-    return dataset
-
-
-def create_trait(dataset, name, cellid):
-    """mock function for creating trait"""
-
-    trait = AttributeSetter({
-        "group": AttributeSetter({
-            "genofile": ""
-        })
-    })
-
-    return trait
-
-
-def get_species_dataset_trait(self_obj, start_vars):
-    # do somethig return
-    """function should somehow mutate the passed object"""
-
-    return ""
+    def get_dict(self):
+        """dummy function  to get dict object"""
+        return self.__dict__
 
 
 def get_genofile_samplelist(dataset):
+    """mock function to get genofile samplelist"""
+
+    print(dataset)
     return ["C57BL/6J"]
