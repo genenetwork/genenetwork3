e8ee1ebec2249324d6a64f2e82a7eee848d86996
Alexander Kabui <alexanderkabua@gmail.com>
2021-03-10 10:24:48 +0300

    minor fixes for endpoints

--- gn3/app.py
+++ gn3/app.py
@@ -30,7 +30,7 @@ def create_app(config: Union[Dict, str, None] = None) -> Flask:
             app.config.update(config)
         elif config.endswith(".py"):
             app.config.from_pyfile(config)
-    app.register_blueprint(general, url_prefix="api/")
-    app.register_blueprint(gemma, url_prefix="api/gemma")
-    app.register_blueprint(correlation,url_prefix="api/correlation")
+    app.register_blueprint(general, url_prefix="/api/")
+    app.register_blueprint(gemma, url_prefix="/api/gemma")
+    app.register_blueprint(correlation,url_prefix="/api/correlation")
     return app
