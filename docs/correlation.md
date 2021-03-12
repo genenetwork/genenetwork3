78165e78117daeeadb3550fe13d08425c54c716e
Alexander Kabui <alexanderkabua@gmail.com>
2021-03-13 02:51:02 +0300

    update docs

--- docs/correlation.md
+++ docs/correlation.md
@@ -10,7 +10,7 @@
 **To  be noted before  spinning the server for correlation computation\which can be set for example env 
 SQL_URI=mysql://user:password@localhost/db_webqtl and also to GENENETWORK_FILES default is HOME+"/data/genotype_files**
 
-(required  input data **should be in json format**)
+(required  input data *should be in json format*)
 - "primary_samples": "",
 - "trait_id"
 - "dataset"
@@ -27,6 +27,15 @@ SQL_URI=mysql://user:password@localhost/db_webqtl and also to GENENETWORK_FILES 
 - "max_loc_mb"
 - "p_range_lower"
 - "p_range_upper"
+
+- example
+
+```bash
+curl -X POST -H "Content-Type: application/json" \
+    -d '{"primary_samles":"",trait_id:"","dataset":"","sample_vals":"","corr_type":"",corr_sample_group:"",corr_sample_method:""}' \
+    localhost:5000/api/correlation/correlation_compute
+
+ ```
 
 
 - output data is correlation_json 
--- tests/integration/test_correlation.py
+++ tests/integration/test_correlation.py
@@ -36,91 +36,10 @@ class CorrelationAPITest(unittest.TestCase):
         self.correlation_results = ""
 
     @mock.patch("gn3.api.correlation.compute_correlation")
-    def test_corr_compute(self,compute_corr):
+    def test_corr_compute(self, compute_corr):
         """Test that the correct response in correlation"""
 
         compute_corr.return_value = self.correlation_results
-
-        sample_vals = """{"C57BL/6J":"7.197","DBA/2J":"7.148","B6D2F1":"6.999"}"""
-        _post_data = {
-            "corr_return_results": 100,
-            "corr_sample_method": "pearson",
-            "corr_type": "sample",
-            "primary_samples": "C57BL/6J,DBA/2J,B6D2F1",
-            "sample_vals": sample_vals,
-            "dataset": "Temp",
-            "group": "G1",
-            "corr_dataset": "D1",
-            "corr_samples_group": "samples_primary",
-            "n_samples": 71,
-            "min_expr": "",
-            "p_range_lower": "-1.00",
-            "p_range_upper": "1.00",
-            "trait_id": "1444666_at",
-            "wanted_inputs": "primary_samples,sample_vals,p_range_lower,\
-            p_range_upper,min_expr,corr_samples_group,corr_dataset,\
-            n_samples,corr_type,corr_sample_method,corr_return_results,trait_id,group,dataset"
-        }
-
-        _expected_data_correlation = {
-            "corr_method": "pearson",
-            'formatted_corr_type': "Genetic Correlation (Pearson's r)",
-            "corr_type": "sample",
-            "dataset": {
-                "group": {
-
-                    "genofile": "",
-                    "samplelist": "S1",
-                    "parlist": "",
-                    "f1list": ""
-
-                }
-            },
-
-            "location_chr": None,
-            "location_type": None,
-            "max_location_mb": None,
-            "min_expr": None,
-            "min_location_mb": None,
-            "p_range_lower": -1.0,
-            "p_range_upper": 1.0,
-            "return_number": 100,
-            "sample_data": {
-
-            },
-            "this_trait": {
-                "group": {
-                    "genofile": ""
-                }
-
-            },
-
-            "trait_id": "1444666_at"
-
-
-
-        }
-
-        _expected_data_loading = {
-            "start_vars": {
-                "corr_dataset": "D1",
-                "corr_return_results": 100,
-                "corr_sample_method": "pearson",
-                "corr_type": "sample",
-                "dataset": "Temp",
-                "group": "G1",
-                "n_samples": 71,
-                "p_range_lower": "-1.00",
-                "p_range_upper": "1.00",
-                "min_expr": None,
-                "corr_samples_group": "samples_primary",
-                "trait_id": "1444666_at",
-                "wanted_inputs": "p_range_lower,p_range_upper,\
-                min_expr,corr_samples_group,corr_dataset,\
-                n_samples,corr_type,corr_sample_method,corr_return_results,trait_id,group,dataset"
-            }
-        }
-
         response = self.app.post(
             "/api/correlation/corr_compute", json=self.correlation_data, follow_redirects=True)
 
