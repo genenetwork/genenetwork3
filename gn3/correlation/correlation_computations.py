c27d62c9749d294554ceff95e5f684a499c8c738
Alexander Kabui <alexanderkabua@gmail.com>
2021-03-13 01:31:14 +0300

    delete filter_input function
    update test and data for correlation

--- gn3/correlation/correlation_computations.py
+++ gn3/correlation/correlation_computations.py
@@ -9,60 +9,59 @@ def filter_wanted_inputs():
     raise NotImplementedError()
 
 
-def filter_input_data(initial_start_vars):
-    """functional to filter form/json data and create_dataset and trait"""
-    if initial_start_vars is None:
-        # added this just to enable testing of this function
-        raise NotImplementedError()
+# def filter_input_data(initial_start_vars):
+#     """functional to filter form/json data and create_dataset and trait"""
+#     if initial_start_vars is None:
+#         # added this just to enable testing of this function
+#         raise NotImplementedError()
 
-    start_vars_container = {}
-    n_samples = 0
-    if "wanted_inputs" in initial_start_vars:
-        wanted = initial_start_vars["wanted_inputs"].split(",")
-        start_vars = {}
+#     start_vars_container = {}
+#     n_samples = 0
+#     if "wanted_inputs" in initial_start_vars:
+#         wanted = initial_start_vars["wanted_inputs"].split(",")
+#         start_vars = {}
 
-        for key, value in initial_start_vars.items():
-            if key in wanted:
-                start_vars[key] = value
+#         for key, value in initial_start_vars.items():
+#             if key in wanted:
+#                 start_vars[key] = value
 
-        if "n_samples" in start_vars:
-            n_samples = int(start_vars["n_samples"])
+#         # if "n_samples" in start_vars:
+#         #     n_samples = int(start_vars["n_samples"])
 
-        else:
-            sample_vals_dict = json.loads(start_vars['sample_vals'])
-            samples = start_vars['primary_samples'].split(",")
+#         # else:
+#         #     sample_vals_dict = json.loads(start_vars['sample_vals'])
+#         #     samples = start_vars['primary_samples'].split(",")
 
-            for sample in samples:
-                if sample in sample_vals_dict:
-                    if sample_vals_dict[sample] != "x":
-                        n_samples += 1
+#         #     for sample in samples:
+#         #         if sample in sample_vals_dict:
+#         #             if sample_vals_dict[sample] != "x":
+#         #                 n_samples += 1
 
-        start_vars['n_samples'] = n_samples
-        start_vars['wanted_inputs'] = initial_start_vars['wanted_inputs']
+#         # start_vars['n_samples'] = n_samples
+#         start_vars['wanted_inputs'] = initial_start_vars['wanted_inputs']
 
-        start_vars_container['start_vars'] = start_vars
+#         start_vars_container['start_vars'] = start_vars
 
-    else:
+#     else:
 
-        start_vars_container['start_vars'] = initial_start_vars
+#         start_vars_container['start_vars'] = initial_start_vars
 
-    return start_vars_container
+#     return start_vars_container
 
 
-def compute_correlation(init_start_vars,
-                        get_input_data=filter_input_data,
+def compute_correlation(correlation_input_data,
                         correlation_results=CorrelationResults):
     """function that does correlation .creates Correlation results instance"""
 
-    start_vars_container = get_input_data(
-        initial_start_vars=init_start_vars)
+    # start_vars_container = get_input_data(
+    #     initial_start_vars=init_start_vars)
 
-    start_vars = start_vars_container["start_vars"]
+    # start_vars = start_vars_container["start_vars"]
 
     corr_object = correlation_results(
-        start_vars=start_vars)
+        start_vars=correlation_input_data)
 
-    corr_results = corr_object.do_correlation(start_vars=start_vars)
+    corr_results = corr_object.do_correlation(start_vars=correlation_input_data)
     # possibility of file being so large cause of the not sure whether to return a file
 
     return corr_results
--- tests/unit/correlation/test_correlation_computations.py
+++ tests/unit/correlation/test_correlation_computations.py
@@ -1,17 +1,13 @@
 """module for testing correlation/correlation_computations"""
 
 import unittest
-from gn3.correlation.correlation_computations import filter_input_data
 from gn3.correlation.correlation_computations import compute_correlation
-
-
 
 
 # mock for calculating correlation function
 
 def mock_get_loading_page_data(initial_start_vars):
     """function to mock  filtering input"""
-    print(initial_start_vars)
     results = {'start_vars':
                {'genofile': 'SAMPLE:X', 'dataset': 'HC_M2_0606_P',
                 'sample_vals': '{"C57BL/6J":"7.197","DBA/2J":"7.148","B6D2F1":"6.999"}',
@@ -38,62 +34,21 @@ class MockCorrelationResults:
 
     def do_correlation(self, start_vars):
         """mock method for doing correlation"""
-        print(self.__class__.__name__)
 
         return {
-            "success": start_vars
+            "results": "success"
         }
 
 
 class TestCorrelationUtility(unittest.TestCase):
     """tests for correlation computations"""
 
-    def test_fails(self):
-        """add test that fails"""
-
-        self.assertEqual(4, 4)
-
-    def test_filter_input_no_data(self):
-        """test loading page data function where initial start vars is None"""
-        with self.assertRaises(NotImplementedError):
-            results = filter_input_data(initial_start_vars=None)
-            self.assertEqual(results, None)
-
-    def test_filter_input_data(self):
-        '''testing getting loading page data when n_samples key exists'''
-        sample_vals = """{"C57BL/6J":"7.197","DBA/2J":"7.148","B6D2F1":"6.999"}"""
-
-        initial_start_vars = {
-
-            "n_samples": "71",
-            "wanted_inputs": "n_samples",
-
-            "sample_vals": sample_vals,
-            "primary_samples": "C57BL/6J,DBA/2J,B6D2F1"
-
-        }
-
-        results = filter_input_data(initial_start_vars=initial_start_vars)
-
-        expected_starts_vars = {
-            "n_samples": 71,
-            "wanted_inputs": "n_samples",
-
-        }
-
-        expected_starts_vars_container = {
-            "start_vars": expected_starts_vars
-        }
-
-        self.assertEqual(expected_starts_vars, results["start_vars"])
-        self.assertEqual(expected_starts_vars_container, results)
-
     def test_compute_correlation(self):
         """test function for doing correlation"""
 
         sample_vals = """{"C57BL/6J":"7.197","DBA/2J":"7.148","B6D2F1":"6.999"}"""
 
-        initial_start_vars = {
+        correlation_input_data = {
             "wanted_inputs": "sample_vals,dataset,genofile,primary_samples",
             "genofile": "SAMPLE:X",
             "dataset": "HC_M2_0606_P",
@@ -102,17 +57,9 @@ class TestCorrelationUtility(unittest.TestCase):
             "primary_samples": "C57BL/6J,DBA/2J,B6D2F1"
 
         }
-        correlation_object = compute_correlation(
-            init_start_vars=initial_start_vars,
-            get_input_data=mock_get_loading_page_data,
+        correlation_results = compute_correlation(
+            correlation_input_data=correlation_input_data,
             correlation_results=MockCorrelationResults)
+        results = {"results": "success"}
 
-        results = {'genofile': 'SAMPLE:X', 'dataset': 'HC_M2_0606_P',
-                   'sample_vals': '{"C57BL/6J":"7.197","DBA/2J":"7.148","B6D2F1":"6.999"}',
-                   'primary_samples': 'C57BL/6J,DBA/2J,B6D2F1',
-                   'n_samples': 3,
-                   'wanted_inputs': "sample_vals,dataset,genofile,primary_samples"}
-
-        self.assertEqual({
-            "success": results
-        }, correlation_object)
+        self.assertEqual(results,correlation_results)
