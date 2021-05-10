c2ef963c2f0f8a4c9428d48abed1094ff0441935
BonfaceKilz <me@bonfacemunyoki.com>
2021-05-10 21:07:32 +0300

    db: species: Add method for fetching all species

--- gn3/db/species.py
+++ gn3/db/species.py
@@ -6,6 +6,14 @@ from typing import Any, Optional, Tuple
 from MySQLdb import escape_string
 
 
+def get_all_species(conn: Any) -> Optional[Tuple]:
+    """Return a list of all species"""
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT Name, MenuName FROM Species "
+                       "ORDER BY OrderId")
+        return cursor.fetchall()
+
+
 def get_chromosome(name: str, is_species: bool, conn: Any) -> Optional[Tuple]:
     """Given either a group or a species Name, return all the species"""
     _sql = ("SELECT Chr_Length.Name, Chr_Length.OrderId, "
