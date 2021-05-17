7ed84670c0d13de38b578a4e4177b2529ff3fb40
zsloan <zachary.a.sloan@gmail.com>
2021-05-17 21:54:42 +0000

    Read in kwargs fromrequest and pass the command, tmpdir, and kwargs to generate_rqtl_cmd which returns the actual command and output path

--- gn3/api/rqtl.py
+++ gn3/api/rqtl.py
@@ -2,7 +2,11 @@ import os
 
 from flask import Blueprint
 from flask import current_app
+from flask import jsonify
 from flask import request
+
+from gn3.computations.rqtl import generate_rqtl_cmd
+from gn3.computations.gemma import do_paths_exist
 
 rqtl = Blueprint("rqtl", __name__)
 
@@ -16,4 +20,18 @@ def compute():
     if not do_paths_exist([genofile, phenofile]):
         raise FileNotFoundError
 
-    return current_app.config.get("RQTL_WRAPPER_CMD")
+    kwarg_list = ["addcovar", "model", "method", "interval", "nperm", "scale", "control_marker"]
+
+    rqtl_kwargs = {"geno": genofile, "pheno": phenofile}
+    for kwarg in kwarg_list:
+        if kwarg in request.form:
+            rqtl_kwargs[kwarg] = request.form[kwarg]
+
+    results = generate_rqtl_cmd(
+        rqtl_wrapper_cmd = current_app.config.get("RQTL_WRAPPER_CMD"),
+        output_dir = current_app.config.get('TMPDIR'),
+        rqtl_wrapper_kwargs = rqtl_kwargs
+    )
+
+    return jsonify(results)
+
