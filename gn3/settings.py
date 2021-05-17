ea9d9d6454783a6af07384a66f204e199035e5bd
zsloan <zachary.a.sloan@gmail.com>
2021-05-17 21:52:43 +0000

    Added RQTL_WRAPPER_CMD (which is fetched from environment) in settings.py

--- gn3/settings.py
+++ gn3/settings.py
@@ -6,6 +6,7 @@ import os
 BCRYPT_SALT = "$2b$12$mxLvu9XRLlIaaSeDxt8Sle"  # Change this!
 DATA_DIR = ""
 GEMMA_WRAPPER_CMD = os.environ.get("GEMMA_WRAPPER", "gemma-wrapper")
+RQTL_WRAPPER_CMD = os.environ.get("RQTL_WRAPPER")
 CACHEDIR = ""
 REDIS_URI = "redis://localhost:6379/0"
 REDIS_JOB_QUEUE = "GN3::job-queue"
