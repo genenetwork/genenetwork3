856d4a72ace3584b731a7c45470e3df2704be02f
BonfaceKilz <me@bonfacemunyoki.com>
2021-05-19 22:12:45 +0300

    db: phenotypes: Update failing tests

--- tests/unit/db/test_phenotypes.py
+++ tests/unit/db/test_phenotypes.py
@@ -3,19 +3,20 @@ from unittest import TestCase
 from unittest import mock
 
 from gn3.db.phenotypes import Phenotype
-from gn3.db.phenotypes import update_phenotype
+from gn3.db.phenotypes import update
 
 
 class TestPhenotypes(TestCase):
     """Test cases for fetching chromosomes"""
     def test_update_phenotype_with_no_data(self):
-        """
-        Test that a phenotype is updated correctly if an empty Phenotype dataclass
+        """Test that a phenotype is updated correctly if an empty Phenotype dataclass
         is provided
+
         """
         db_mock = mock.MagicMock()
-        self.assertEqual(update_phenotype(
-            db_mock, data=Phenotype(), where=Phenotype()), None)
+        self.assertEqual(update(
+            conn=db_mock, table="Phenotype",
+            data=Phenotype(), where=Phenotype()), None)
 
     def test_update_phenotype_with_data(self):
         """
@@ -25,8 +26,9 @@ class TestPhenotypes(TestCase):
         db_mock = mock.MagicMock()
         with db_mock.cursor() as cursor:
             type(cursor).rowcount = 1
-            self.assertEqual(update_phenotype(
-                db_mock, data=Phenotype(
+            self.assertEqual(update(
+                conn=db_mock, table="Phenotype",
+                data=Phenotype(
                     pre_pub_description="Test Pre Pub",
                     submitter="Rob",
                     post_pub_description="Test Post Pub"),
