bf0a8cb22c0cc0c1dfe25740f88a4cb159dd0064
zsloan <zachary.a.sloan@gmail.com>
2021-05-25 20:27:33 +0000

    Fix R/qtl command and the way keyword arguments are passed

--- gn3/commands.py
+++ gn3/commands.py
@@ -35,10 +35,10 @@ def compose_rqtl_cmd(rqtl_wrapper_cmd: str,
                      rqtl_wrapper_bool_kwargs: list) -> str:
     """Compose a valid R/qtl command given the correct input"""
     # Add kwargs with values
-    cmd = rqtl_wrapper_cmd + " " + " ".join(
+    cmd = f"Rscript { rqtl_wrapper_cmd } " + " ".join(
         [f"--{key} {val}" for key, val in rqtl_wrapper_kwargs.items()])
 
-    # Add boolean kwargs (kwargs without values)
+    # Add boolean kwargs (kwargs that are either on or off, like --interval)
     if rqtl_wrapper_bool_kwargs:
         cmd += " "
         cmd += " ".join([f"--{val}" for val in rqtl_wrapper_bool_kwargs])
