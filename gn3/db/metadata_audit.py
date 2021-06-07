61cf4be04f1382ead04e9c1f7d5fffdfc395d6a4
BonfaceKilz <me@bonfacemunyoki.com>
2021-06-07 18:53:59 +0300

    gn3: db: Add "id_" property to metadata_audit class and mapping

--- gn3/db/metadata_audit.py
+++ gn3/db/metadata_audit.py
@@ -10,6 +10,7 @@ from typing import Optional
 @dataclass(frozen=True)
 class MetadataAudit:
     """Data Type that represents a Phenotype"""
+    id_: Optional[int] = None
     dataset_id: Optional[int] = None
     editor: Optional[str] = None
     json_data: Optional[str] = None
@@ -19,6 +20,7 @@ class MetadataAudit:
 # Mapping from the MetadataAudit dataclass to the actual column names in the
 # database
 metadata_audit_mapping = {
+    "id_": "id",
     "dataset_id": "dataset_id",
     "editor": "editor",
     "json_data": "json_data",
