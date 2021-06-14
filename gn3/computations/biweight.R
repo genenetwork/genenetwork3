f9dec15fb4a1fc24dfb963ed69f2a5cbbd4f31d3
Alexander Kabui <alexanderkabua@gmail.com>
2021-06-14 13:50:21 +0300

    fix missing call to bicorAndPvalue

--- gn3/computations/biweight.R
+++ gn3/computations/biweight.R
@@ -7,7 +7,7 @@ trait_vals <- as.numeric(unlist(strsplit(myArgs[1], split=" ")))
 target_vals <- as.numeric(unlist(strsplit(myArgs[2], split=" ")))
 
 BiweightMidCorrelation <- function(trait_val,target_val){
-    # results <- bicorAndPvalue(x,y)
+    results <- bicorAndPvalue(x,y)
     return (list(c(results$bicor)[1],c(results$p)[1]))
 }
 cat(BiweightMidCorrelation(trait_vals,target_vals))
