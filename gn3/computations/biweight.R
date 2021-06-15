c708f435f14e5394aa4e6e8f556aec976d61341f
Alexander Kabui <alexanderkabua@gmail.com>
2021-06-15 08:08:38 +0300

    fix script path issues

--- gn3/computations/biweight.R
+++ gn3/computations/biweight.R
@@ -1,5 +1,5 @@
 
-library(WGCNA)
+# library(WGCNA)
 
 
 myArgs <- commandArgs(trailingOnly = TRUE)
@@ -7,9 +7,22 @@ trait_vals <- as.numeric(unlist(strsplit(myArgs[1], split=" ")))
 target_vals <- as.numeric(unlist(strsplit(myArgs[2], split=" ")))
 
 BiweightMidCorrelation <- function(trait_val,target_val){
-    results <- bicorAndPvalue(x,y)
+    results <- bicorAndPvalue(trait_val,target_val)
     return (list(c(results$bicor)[1],c(results$p)[1]))
 }
-cat(BiweightMidCorrelation(trait_vals,target_vals))
 
+
+
+
+
+
+# the idea is that you get the entire dataset in any format 
+# and then do ther correlation
+
+ComputeAll <-function(trait_val,target_dataset) {
+	for target_val in target_dataset {
+      results = BiweightMidCorrelation(trait_val,target_val)
+      cat(BiweightMidCorrelation(trait_vals,target_vals))
+	}
+}
 
--- gn3/computations/biweight.py
+++ gn3/computations/biweight.py
@@ -2,17 +2,21 @@
 
 """module contains script to call biweight mid\
 correlation in R"""
-
 import subprocess
+import os
+from pathlib import Path
 from typing import List
+
+FILE_PATH = os.path.join(Path(__file__).parent.absolute(), "biweight.R")
 
 
 def call_biweight_script(trait_vals: List,
                          target_vals: List,
-                         path_to_script: str = "./biweight_R",
+                         path_to_script: str = FILE_PATH,
                          command: str = "Rscript"
                          ):
     '''biweight function'''
+
     args_1 = ' '.join(str(trait_val) for trait_val in trait_vals)
     args_2 = ' '.join(str(target_val) for target_val in target_vals)
     cmd = [command, path_to_script] + [args_1] + [args_2]
