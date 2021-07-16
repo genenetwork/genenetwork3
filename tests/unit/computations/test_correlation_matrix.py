93fc0bd7b9433d46dfb9f7f81549c46015a3d0c7
Alexander Kabui <alexanderkabua@gmail.com>
2021-07-16 11:59:59 +0300

    unittest for tfetching sample datas

---
+++ tests/unit/computations/test_correlation_matrix.py
@@ -0,0 +1,43 @@
+"""module contains unittest for correlatiom matrix"""
+
+import unittest
+from types import SimpleNamespace
+from gn3.computations.correlation_matrix import fetch_sample_datas
+
+
+class TestCorrelationMatrix(unittest.TestCase):
+    """test for correlation matrix functions"""
+
+    def test_fetch_sample_datas(self):
+        """function to fetch both trait_vals and target_vals"""
+
+        # target_sample and also this_sample
+
+        # target_samples_keys ==target_samples ??????????????
+
+        target_samples = ["C57BL/6J", "DBA/2J", "B6D2F1",
+                          "D2B6F1", "BXD1", "BXD2", "BXD5", "BXD6"]
+
+        target_samples_data = {"C57BL/6J": SimpleNamespace(value=1.2),
+                               "B6D2F1": SimpleNamespace(value=2.1),
+                               "BXD": SimpleNamespace(value=1.1),
+                               "BXD12": SimpleNamespace(value=2.2),
+                               "BXD5": SimpleNamespace(value=1.2),
+                               "BXD6": SimpleNamespace(value=1.1)}
+
+        this_samples_data = {"DBA/2J": SimpleNamespace(value=1.22),
+                             "B6D2F1": SimpleNamespace(value=2.3),
+                             "BXD1":    SimpleNamespace(value=1.1),
+                             "BXD2": SimpleNamespace(value=2.2),
+                             "BXD5": SimpleNamespace(value=1.2),
+                             "BXD6": SimpleNamespace(value=1.1)}
+
+        expected_target_trait_vals = [2.1, 1.2, 1.1]
+
+        expected_this_trait_vals = [2.3, 1.2, 1.1]
+
+        results = fetch_sample_datas(
+            target_samples, target_samples_data, this_samples_data)
+
+        self.assertEqual(results, (expected_this_trait_vals,
+                                   expected_target_trait_vals))
