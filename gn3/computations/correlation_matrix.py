e33e2fe7f289df03b1dc55a34b68622de6b586ea
Alexander Kabui <alexanderkabua@gmail.com>
2021-07-16 11:59:28 +0300

    function to fetch trait_vals and sample_vals

--- gn3/computations/correlation_matrix.py
+++ gn3/computations/correlation_matrix.py
@@ -1,5 +1,8 @@
 """module contains code for computing pca using sklearn"""
 
+from typing import List
+from typing import Tuple
+
 import numpy as np
 from numpy import linalg
 
@@ -56,13 +59,41 @@ def compute_zscores(trait_data_arrays):
     return zscores
 
 
-def compute_eigens(matrix):
+def compute_sort_eigens(matrix):
     """compute eigen values and vectors sort by eigen values"""
+
     eigen_values, eigen_vectors = linalg.eig(matrix)
 
     idx = eigen_values.argsort()[::-1]
     eigen_values = eigen_values[idx]
 
     eigen_vectors = eigen_vectors[:, idx]
-
     return (eigen_values, eigen_vectors)
+
+
+def fetch_sample_datas(target_samples: List,
+                       target_sample_data: dict,
+                       this_samples_data: dict) ->Tuple[List[float], List[float]]:
+    """get shared sample data xtodo refactor to use correlation"""
+
+    # xtodo refactor correlation signature in /computation/correlation
+
+    # intenal of tsamples = ["BXD1","BXD2","BXD3"]
+    # internal representaito of sample data is {"bxd":obj(val),bxd:obj(val)}
+
+    target_trait_vals = []
+    this_trait_vals = []
+
+    shared_samples = set(target_sample_data).intersection(this_samples_data)
+
+    for sample in target_samples:
+        if sample in shared_samples:
+            target_trait_vals.append(target_sample_data[sample].value)
+            this_trait_vals.append(this_samples_data[sample].value)
+
+        else:
+            # remove sample key from shared
+            pass
+
+    return (this_trait_vals, target_trait_vals)
+    #
