8dc60fd93b9eab9fd655a1f0c23ed038d4ac3eed
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2021-07-20 15:12:38 +0300

    Implement the correlation function
    Issue:
    https://github.com/genenetwork/gn-gemtext-threads/blob/main/topics/gn1-migration-to-gn2/clustering.gmi
    
    * Implement the correlation computation function, such that it passes the
      tests created previously.

--- gn3/computations/correlations2.py
+++ gn3/computations/correlations2.py
@@ -1,4 +1,35 @@
+from math import sqrt
+from functools import reduce
 ## From GN1: mostly for clustering and heatmap generation
 
+def items_with_values(dbdata, userdata):
+    """Retains only corresponding items in the data items that are not `None` values.
+This should probably be renamed to something sensible"""
+    def both_not_none(item1, item2):
+        if (item1 is not None) and (item2 is not None):
+            return (item1, item2)
+        return None
+    def split_lists(accumulator, item):
+        return [accumulator[0] + [item[0]], accumulator[1] + [item[1]]]
+    return reduce(
+        split_lists,
+        filter(lambda x: x is not None, map(both_not_none, dbdata, userdata)),
+        [[], []])
+
 def compute_correlation(dbdata, userdata):
-    return tuple()
+    x, y = items_with_values(dbdata, userdata)
+    if len(x) < 6:
+        return (0.0, len(x))
+    meanx = sum(x)/len(x)
+    meany = sum(y)/len(y)
+    def cal_corr_vals(acc, item):
+        xitem, yitem = item
+        return [
+            acc[0] + ((xitem - meanx) * (yitem - meany)),
+            acc[1] + ((xitem - meanx) * (xitem - meanx)),
+            acc[2] + ((yitem - meany) * (yitem - meany))]
+    xyd, sxd, syd = reduce(cal_corr_vals, zip(x, y), [0.0, 0.0, 0.0])
+    try:
+        return ((xyd/(sqrt(sxd)*sqrt(syd))), len(x))
+    except ZeroDivisionError as zde:
+        return(0, len(x))
