830b4b1f36d46b230544175169262b03526720c1
Muriithi Frederick Muriuki <fredmanglis@gmail.com>
2021-07-22 11:43:39 +0300

    Implement check for lists or tuples
    Issue:
    https://github.com/genenetwork/gn-gemtext-threads/blob/main/topics/gn1-migration-to-gn2/clustering.gmi
    
    * Implement the code to pass the check that a list of lists is passed to the
      `nearest' function.

--- gn3/computations/slink.py
+++ gn3/computations/slink.py
@@ -4,5 +4,14 @@ class LengthError(BaseException):
 class MirrorError(BaseException):
     pass
 
+def raise_valueerror_if_data_is_not_lists_or_tuples(lists):
+    """Check that `lists` is a list of lists: If not, raise an exception."""
+    def is_list_or_tuple(item):
+        return type(item) == type([]) or type(item) == type(tuple)
+
+    if (not is_list_or_tuple(lists)) or (not all(map(is_list_or_tuple, lists))):
+        raise ValueError("Expected list or tuple")
+
 def nearest(lists, i, j):
+    raise_valueerror_if_data_is_not_lists_or_tuples(lists)
     return None
