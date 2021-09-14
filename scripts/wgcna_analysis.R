49fdd614d4b18f7d28126e4ebbf3adca57f0416f
Alexander Kabui <alexanderkabua@gmail.com>
2021-09-14 22:31:18 +0300

    Checking data for excessive missing values

--- scripts/wgcna_analysis.R
+++ scripts/wgcna_analysis.R
@@ -10,9 +10,27 @@ wgcnaRawData <- read.csv(file = "wgcna_data.csv")
 
 # transform expressionData
 
-datExpr <- as.data.frame(t(wgcnaRawData));
+dataExpr <- as.data.frame(t(wgcnaRawData));
 
 
+
+# data cleaning
+
+# adopted from docs
+gsg = goodSamplesGenes(dataExpr, verbose = 3);
+
+
+
+if (!gsg$allOK)
+{
+# Optionally, print the gene and sample names that were removed:
+if (sum(!gsg$goodGenes)>0)
+printFlush(paste("Removing genes:", paste(names(datExpr0)[!gsg$goodGenes], collapse = ", ")));
+if (sum(!gsg$goodSamples)>0)
+printFlush(paste("Removing samples:", paste(rownames(datExpr0)[!gsg$goodSamples], collapse = ", ")));
+# Remove the offending genes and samples from the data:
+dataExpr <- dataExpr[gsg$goodSamples, gsg$goodGenes]
+}
 
 
 
