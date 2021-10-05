453570cbf22a92de72fa0401895c99a923831942
Alexander Kabui <alexanderkabua@gmail.com>
2021-10-05 15:14:25 +0300

    script modification : add debug statements

--- scripts/wgcna_analysis.R
+++ scripts/wgcna_analysis.R
@@ -6,10 +6,14 @@ library(rjson)
 
 options(stringsAsFactors = FALSE);
 
-imgDir = Sys.getenv("GENERATED_IMAGE_DIR")
+cat("Running the wgcna analysis script\n")
+
 # load expression data **assumes from json files row(traits)(columns info+samples)
 # pass the file_path as arg
 # pass the file path to read json data
+
+
+
 
 args = commandArgs(trailingOnly=TRUE)
 
@@ -21,6 +25,7 @@ if (length(args)==0) {
 }
 
 inputData <- fromJSON(file = json_file_path)
+imgDir = inputData$TMPDIR
 
 
 trait_sample_data <- do.call(rbind, inputData$trait_sample_data)
@@ -83,6 +88,11 @@ network <- blockwiseModules(dataExpr,
 
 
 
+cat("Generated network \n")
+
+network
+
+
 genImageRandStr <- function(prefix){
 
 	randStr <- paste(prefix,stri_rand_strings(1, 9, pattern = "[A-Za-z0-9]"),sep="_")
@@ -94,6 +104,10 @@ mergedColors <- labels2colors(network$colors)
 
 imageLoc <- file.path(imgDir,genImageRandStr("WGCNAoutput"))
 png(imageLoc,width=1000,height=600,type='cairo-png')
+
+
+cat("Generating the CLuster  dendrogram\n")
+
 
 plotDendroAndColors(network$dendrograms[[1]],mergedColors[network$blockGenes[[1]]],
 "Module colors",
