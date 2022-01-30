100169674f048f07d315a84fe9b3d17fd55292c8
Alexander Kabui <alexanderkabua@gmail.com>
2022-01-31 00:37:33 +0300

    pep8 formatting

--- gn3/api/ctl.py
+++ gn3/api/ctl.py
@@ -1,6 +1,6 @@
 """module contains endpoints for ctl"""
 
-# so small 
+# so small
 
 from flask import Blueprint
 from flask import request
@@ -8,16 +8,17 @@ from flask import jsonify
 
 from gn3.computations.ctl import call_ctl_script
 
-ctl = Blueprint("ctl",__name__)
+ctl = Blueprint("ctl", __name__)
 
-@ctl.route("/run_ctl",methods=["POST"])
+
+@ctl.route("/run_ctl", methods=["POST"])
 def run_ctl():
-	"""endpoint to run ctl"""
-	ctl_data = request.json
+    """endpoint to run ctl"""
+    ctl_data = request.json
 
-	results = call_ctl_script(ctl_data)
+    results = call_ctl_script(ctl_data)
 
-	return jsonify({
-	"results":results
-	})
-
+    return jsonify({
+        "results": results
+    })
+    
