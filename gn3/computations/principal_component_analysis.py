52197fd0875d4a1bbc29e1556fc477d197e50a80
Alexander Kabui <alexanderkabua@gmail.com>
2022-02-18 12:42:39 +0300

    function to compute pca

--- gn3/computations/principal_component_analysis.py
+++ gn3/computations/principal_component_analysis.py
@@ -2,6 +2,21 @@
 
 import numpy
 from scipy import stats
+from sklearn.decomposition import PCA
+
+
+def compute_pca(matrix):
+
+    pca = PCA()
+    scaled_data = preprocessing.scale(matrix)
+
+    pca.fit(scaled_data)
+
+    return {
+        "pca": pca
+        "components": pca_obj.components_,
+        "scores": pca.transform(scaled_data)
+    }
 
 
 def compute_zscores(nums: list, axis: int = 0, ddof: int = 0) -> list:
