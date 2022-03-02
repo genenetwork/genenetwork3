68afe541610f9a395d110fd5cb2552aaebc59a96
Alexander Kabui <alexanderkabua@gmail.com>
2022-03-02 18:57:41 +0300

    refactor tests

--- tests/unit/computations/test_principal_component_analysis.py
+++ tests/unit/computations/test_principal_component_analysis.py
@@ -4,10 +4,11 @@ from unittest.mock import patch
 from unittest.mock import Mock
 
 import numpy as np
-from gn3.computations.principal_component_analysis import compute_pca
+
 from gn3.computations.principal_component_analysis import process_factor_loadings_tdata
 from gn3.computations.principal_component_analysis import generate_pca_temp_dataset
 from gn3.computations.principal_component_analysis import cache_pca_dataset
+from gn3.computations.principal_component_analysis import generate_scree_plot_data
 
 
 class TestPCA(unittest.TestCase):
@@ -30,40 +31,37 @@ class TestPCA(unittest.TestCase):
         self.assertEqual(process_factor_loadings_tdata(
             test_array, 3), expected_results)
 
-    @patch("gn3.computations.principal_component_analysis.generate_pca_traits_data")
+    @patch("gn3.computations.principal_component_analysis.generate_pca_traits_vals")
     def test_generate_pca_datasets(self, mock_pca_data):
         """test for generating temp pca dataset"""
 
-        mock_pca_data.return_value = [[21, 10, 17, 15, 13],
-                                      [21, 11, 18,
-                                       9, 1],
-                                      [22, 16, 0,
-                                       0.22667229, -1],
-                                      [31, 12, 10, 17, 11]]
+        mock_pca_data.return_value = np.array([[21, 10, 17, 15, 13],
+                                               [21, 11, 18,
+                                                9, 1],
+                                               [22, 16, 0,
+                                                0.22667229, -1],
+                                               [31, 12, 10, 17, 11]])
 
         shared_samples = ["BXD1", "BXD2", "BXD", "BXD4", "Unkown"]
 
         dataset_samples = ["BXD1", "BXD5", "BXD4", "BXD"]
-        expected_results = {"PCA1-mouseG1now": [21,   'x',   10,   17],
-                            'PCA2-mouseG1now': [21,   'x',   11,   18],
-                            'PCA3-mouseG1now': [22,   'x',   16,   0],
-                            'PCA4-mouseG1now': [31,   'x',   12,   10]}
+        expected_results = {"PCA1_mouse_G1_now": ["21.0",   'x',   "10.0",   "17.0"],
+                            'PCA2_mouse_G1_now': ["21.0",   'x',   "11.0",   "18.0"],
+                            'PCA3_mouse_G1_now': ["22.0",   'x',   "16.0",   "0.0"],
+                            'PCA4_mouse_G1_now': ["31.0",   'x',   "12.0",   "10.0"]}
 
         self.assertEqual(generate_pca_temp_dataset(species="mouse", group="G1",
                                                    traits_data=[],
                                                    dataset_samples=dataset_samples,
+                                                   corr_array=[],
                                                    shared_samples=shared_samples,
                                                    create_time="now"), expected_results)
 
-    def test_cache_pca_datasets(self):
-        """test for parsing and caching pca datasets"""
+    def test_generate_scree_plot(self):
+        """test scree plot data is generated"""
 
-        mock_redis = Mock()
-        mock_redis.set.return_value = None
+        variance = [0.9271, 0.06232, 0.031]
 
-        pca_trait_dict = {"PCA1-mouseG1now": [21,   'x',   10,   17],
-                          'PCA2-mouseG1now': [21,   'x',   11,   18],
-                          'PCA3-mouseG1now': [22,   'x',   16,   0],
-                          'PCA4-mouseG1now': [31,   'x',   12,   10]}
+        expected_results = [('PC1', 92.7), ('PC2', 6.2), ('PC3', 3.1)]
 
-        self.assertIs(cache_pca_dataset(mock_redis, 30, pca_trait_dict), True)
+        self.assertEqual(generate_scree_plot_data(variance), expected_results)
