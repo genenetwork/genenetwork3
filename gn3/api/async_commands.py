84f51f48a59da93e287d793d983ace4d06ccb483
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2022-03-04 08:31:42 +0300

    Automatically decode Redis strings

--- gn3/api/async_commands.py
+++ gn3/api/async_commands.py
@@ -8,14 +8,11 @@ async_commands = Blueprint("async_commands", __name__)
 
 @async_commands.route("/state/<command_id>")
 def command_state(command_id):
-    with redis.Redis() as rconn:
+    with redis.Redis(decode_responses=True) as rconn:
         state = rconn.hgetall(name=command_id)
         if not state:
             return jsonify(
                 status=404,
                 error="The command id provided does not exist.")
-        state = {
-            key.decode("utf-8"): val.decode("utf8")
-            for key,val in state.items()
-        }
+        state = {key: val for key,val in state.items()}
         return jsonify(state)
