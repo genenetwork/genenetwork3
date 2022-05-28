3e1b3873a4565cc711e5b82cc60d77a61e19e4df
Alexander <alexanderkabua@gmail.com>
2022-05-28 17:49:48 +0300

    init rust correlation module

---
+++ gn3/computations/rust_correlation.py
@@ -0,0 +1,13 @@
+import subprocess
+
+from gn3.settings import CORRELATION_COMMAND
+from gn3.settings import TMPDIR
+
+def run_correlation(file_name:&str,outputdir:str = TMPDIR):
+
+
+	command_list = [CORRELATION_COMMAND, file_name,outputdir]
+
+	results = subprocess.run(command_list, check=True)
+
+	return results
