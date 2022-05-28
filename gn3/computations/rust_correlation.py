881c98f2c2b6da989d7ef16366d972791ed9f9e4
Alexander <alexanderkabua@gmail.com>
2022-05-28 17:52:46 +0300

    pep8 formatting

--- gn3/computations/rust_correlation.py
+++ gn3/computations/rust_correlation.py
@@ -3,11 +3,11 @@ import subprocess
 from gn3.settings import CORRELATION_COMMAND
 from gn3.settings import TMPDIR
 
-def run_correlation(file_name:&str,outputdir:str = TMPDIR):
 
+def run_correlation(file_name: & str, outputdir: str = TMPDIR):
 
-	command_list = [CORRELATION_COMMAND, file_name,outputdir]
+    command_list = [CORRELATION_COMMAND, file_name, outputdir]
 
-	results = subprocess.run(command_list, check=True)
+    results = subprocess.run(command_list, check=True)
 
-	return results
+    return results
