95c06b76b445b5138500a1a019274405cd8eeec5
Alexander_Kabui <alexanderkabua@gmail.com>
2022-08-10 07:42:01 +0300

    remove unnecessary parsing of floats

--- gn3/computations/rust_correlation.py
+++ gn3/computations/rust_correlation.py
@@ -93,6 +93,7 @@ def parse_correlation_output(result_file: str,
     return []
 
 
+
 def get_samples(all_samples: dict[str, str],
                 base_samples: list[str],
                 excluded: list[str]):
