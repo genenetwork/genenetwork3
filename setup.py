06da0390a1de5d0aa8eb6d7a0ed3120e350f8a0b
Arun Isaac <arunisaac@systemreboot.net>
2022-10-18 14:40:47 +0530

    Add xapian indexing script.
    * scripts/index-genenetwork: New file.
    * setup.py (install_requires): Add click, pymonad and xapian-bindings.
    (scripts): Add scripts/index-genenetwork.

--- setup.py
+++ setup.py
@@ -14,18 +14,22 @@ setup(author='Bonface M. K.',
                    'science and machine learning'),
       install_requires=[
           "bcrypt>=3.1.7"
+          "click"
           "Flask==1.1.2"
           "mypy==0.790"
           "mypy-extensions==0.4.3"
           "mysqlclient==2.0.1"
           "numpy==1.20.1"
           "pylint==2.5.3"
+          "pymonad"
           "redis==3.5.3"
           "requests==2.25.1"
           "scipy==1.6.0"
           "plotly==4.14.3"
           "flask-cors==3.0.9"
+          "xapian-bindings"
       ],
+      scripts=["scripts/index-genenetwork"],
       license='GPLV3',
       long_description=long_description(),
       long_description_content_type='text/markdown',
