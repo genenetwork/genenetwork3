11e1ec3f5aaa2489b2e0b2f9f69f6a13c6aaa7ff
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2022-11-08 13:50:03 +0300

    Tests: Generalise testing 'CREATE TABLE' migrations
    * gn3/settings.py: Omit trailing slash
    * tests/unit/auth/test_create_table_migrations.py: Generalise testing
      migrations that create tables.
    * tests/unit/auth/test_create_user_credentials_table.py: delete
    * tests/unit/auth/test_migration_create_users_table.py: delete

---
+++ tests/unit/auth/test_create_table_migrations.py
@@ -0,0 +1,61 @@
+"""Test migrations that create tables"""
+from contextlib import closing
+
+import pytest
+import sqlite3
+
+from gn3.migrations import get_migration, apply_migrations, rollback_migrations
+from tests.unit.auth.conftest import (
+    apply_single_migration, rollback_single_migration, migrations_up_to)
+
+migrations_and_tables = (
+    ("20221103_01_js9ub-initialise-the-auth-entic-oris-ation-database.py",
+     "users"),
+    ("20221103_02_sGrIs-create-user-credentials-table.py", "user_credentials"),
+    ("20221108_01_CoxYh-create-the-groups-table.py", "groups"))
+
+@pytest.mark.unit_test
+@pytest.mark.parametrize("migration_file,the_table", migrations_and_tables)
+def test_create_table(
+        auth_testdb_path, auth_migrations_dir, backend, all_migrations,
+        migration_file, the_table):
+    """
+    GIVEN: A database migration script to create table, `the_table`
+    WHEN: The migration is applied
+    THEN: Ensure that the table `the_table` is created
+    """
+    migration_path=f"{auth_migrations_dir}/{migration_file}"
+    older_migrations = migrations_up_to(migration_path, auth_migrations_dir)
+    apply_migrations(backend, older_migrations)
+    with closing(sqlite3.connect(auth_testdb_path)) as conn, closing(conn.cursor()) as cursor:
+        cursor.execute("SELECT name FROM sqlite_schema WHERE type='table'")
+        result = cursor.fetchall()
+        assert the_table not in [row[0] for row in cursor.fetchall()]
+        apply_single_migration(auth_testdb_path, get_migration(migration_path))
+        cursor.execute("SELECT name FROM sqlite_schema WHERE type='table'")
+        assert the_table in [row[0] for row in cursor.fetchall()]
+
+    rollback_migrations(backend, older_migrations)
+
+@pytest.mark.unit_test
+@pytest.mark.parametrize("migration_file,the_table", migrations_and_tables)
+def test_rollback_create_table(
+        auth_testdb_path, auth_migrations_dir, backend, migration_file,
+        the_table):
+    """
+    GIVEN: A database migration script to create the table `the_table`
+    WHEN: The migration is rolled back
+    THEN: Ensure that the table `the_table` no longer exists
+    """
+    migration_path=f"{auth_migrations_dir}/{migration_file}"
+    older_migrations = migrations_up_to(migration_path, auth_migrations_dir)
+    apply_migrations(backend, older_migrations)
+    with closing(sqlite3.connect(auth_testdb_path)) as conn, closing(conn.cursor()) as cursor:
+        apply_single_migration(auth_testdb_path, get_migration(migration_path))
+        cursor.execute("SELECT name FROM sqlite_schema WHERE type='table'")
+        assert the_table in [row[0] for row in cursor.fetchall()]
+        rollback_single_migration(auth_testdb_path, get_migration(migration_path))
+        cursor.execute("SELECT name FROM sqlite_schema WHERE type='table'")
+        assert "the_table" not in [row[0] for row in cursor.fetchall()]
+
+    rollback_migrations(backend, older_migrations)
