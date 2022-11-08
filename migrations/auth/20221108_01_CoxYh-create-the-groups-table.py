dbd3b240811cb095bc87136501ee846f7b0cb42b
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2022-11-08 13:51:45 +0300

    Add migration to create the groups table.

---
+++ migrations/auth/20221108_01_CoxYh-create-the-groups-table.py
@@ -0,0 +1,18 @@
+"""
+Create the groups table
+"""
+
+from yoyo import step
+
+__depends__ = {'20221103_02_sGrIs-create-user-credentials-table'}
+
+steps = [
+    step(
+        """
+        CREATE TABLE IF NOT EXISTS groups(
+            group_id TEXT PRIMARY KEY NOT NULL,
+            group_name TEXT NOT NULL
+        ) WITHOUT ROWID
+        """,
+        "DROP TABLE IF EXISTS groups")
+]
