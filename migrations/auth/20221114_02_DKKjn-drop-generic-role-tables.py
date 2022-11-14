0cbb54819639ca77266398bb3e4663f74e837d8e
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2022-11-14 11:06:52 +0300

    Migrations: Drop `generic_role*` tables
    Drop the `generic_role*` tables, since what they were envisioned for can be
    handled a different, (arguably) more simple way.
    
    * migrations/auth/20221114_02_DKKjn-drop-generic-role-tables.py: new migration
    * tests/unit/auth/test_migrations_drop_tables.py: test new migration

---
+++ migrations/auth/20221114_02_DKKjn-drop-generic-role-tables.py
@@ -0,0 +1,39 @@
+"""
+Drop 'generic_role*' tables
+"""
+
+from yoyo import step
+
+__depends__ = {'20221114_01_n8gsF-create-generic-role-privileges-table'}
+
+steps = [
+    step(
+        """
+        DROP INDEX IF EXISTS
+            idx_tbl_generic_role_privileges_cols_generic_role_id
+        """,
+        """
+        CREATE INDEX IF NOT EXISTS
+            idx_tbl_generic_role_privileges_cols_generic_role_id
+        ON generic_role_privileges(generic_role_id)
+        """),
+    step(
+        "DROP TABLE IF EXISTS generic_role_privileges",
+        """
+        CREATE TABLE IF NOT EXISTS generic_role_privileges(
+            generic_role_id TEXT NOT NULL,
+            privilege_id TEXT NOT NULL,
+            PRIMARY KEY(generic_role_id, privilege_id),
+            FOREIGN KEY(generic_role_id) REFERENCES generic_roles(role_id),
+            FOREIGN KEY(privilege_id) REFERENCES privileges(privilege_id)
+        ) WITHOUT ROWID
+        """),
+    step(
+        "DROP TABLE IF EXISTS generic_roles",
+        """
+        CREATE TABLE IF NOT EXISTS generic_roles(
+            role_id TEXT PRIMARY KEY,
+            role_name TEXT NOT NULL
+        ) WITHOUT ROWID
+        """)
+]
