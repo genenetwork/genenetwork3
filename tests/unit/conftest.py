baf7980cd6080c8cb4e6c6b585948144273600aa
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2022-11-14 18:02:55 +0300

    tests: split test app and test client
    * The test app and the test client are both needed as fixtures in different
      places

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -8,18 +8,23 @@ import pytest
 from gn3.app import create_app
 
 @pytest.fixture(scope="session")
-def client():
-    """Create a test client fixture for tests"""
+def test_app():
     # Do some setup
     with TemporaryDirectory() as testdir:
         testdb = Path(testdir).joinpath(
             f'testdb_{datetime.now().strftime("%Y%m%dT%H%M%S")}')
-        app = create_app({"AUTH_DB": testdb})
-        app.config.update({"TESTING": True})
+        app = create_app()
+        app.config.update({"TESTING": True, "AUTH_DB": testdb})
         app.testing = True
-        yield app.test_client()
+        yield app
         # Clean up after ourselves
         testdb.unlink(missing_ok=True)
+
+@pytest.fixture(scope="session")
+def client(test_app):
+    """Create a test client fixture for tests"""
+    with test_app.app_context():
+        yield test_app.test_client()
 
 @pytest.fixture(scope="session")
 def test_app_config(client): # pylint: disable=redefined-outer-name
