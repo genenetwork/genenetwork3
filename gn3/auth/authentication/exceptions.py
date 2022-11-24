bac3865f7c0d625f2932e1c3fb001cc6a0048921
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2022-11-24 13:37:27 +0300

    auth: Define base exceptions
    * gn3/auth/authentication/exceptions.py: define `AuthenticationError`
    * gn3/auth/authorisation/exceptions.py: define `AuthorisationError`

---
+++ gn3/auth/authentication/exceptions.py
@@ -0,0 +1,4 @@
+"""Exceptions for authentication"""
+
+class AuthenticationError(Exception):
+    """Base exception class for `gn3.auth.authentication` package."""
---
+++ gn3/auth/authorisation/exceptions.py
@@ -0,0 +1,8 @@
+"""Authorisation exceptions"""
+
+class AuthorisationError(Exception):
+    """
+    Top-level exception for the `gn3.auth.authorisation` package.
+
+    All exceptions in this package should inherit from this class.
+    """
