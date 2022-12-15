9fc7ab5701aaef66ec6fc47e10a2b6421e23f83f
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2022-12-15 08:28:03 +0300

    Enable authentication
    * gn3/auth/authentication/routes.py: Add `/login` endpoint and a function to
      validate the login credentials.
    * gn3/auth/authentication/users.py: add function to fetch user details by
      email

--- gn3/auth/authentication/users.py
+++ gn3/auth/authentication/users.py
@@ -2,8 +2,22 @@
 from uuid import UUID
 from typing import NamedTuple
 
+from pymonad.maybe import Just, Maybe, Nothing
+
+from gn3.auth import db
+
 class User(NamedTuple):
     """Class representing a user."""
     user_id: UUID
     email: str
     name: str
+
+def user_by_email(conn: db.DbConnection, email: str) -> Maybe:
+    with db.cursor(conn) as cursor:
+        cursor.execute("SELECT * FROM users WHERE email=?", (email,))
+        row = cursor.fetchone()
+
+    if row:
+        return Just(User(UUID(row["user_id"]), row["email"], row["name"]))
+
+    return Nothing
