6c76667857d5bbc8db962a551cece3f068074055
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2023-02-03 15:23:38 +0300

    auth: commit missing module.

---
+++ gn3/auth/views.py
@@ -0,0 +1,17 @@
+"""The Auth(oris|entic)ation routes/views"""
+from flask import Blueprint
+
+from .authentication.oauth2.views import auth
+
+from .authorisation.users.views import users
+from .authorisation.roles.views import roles
+from .authorisation.groups.views import groups
+from .authorisation.resources.views import resources
+
+oauth2 = Blueprint("oauth2", __name__)
+
+oauth2.register_blueprint(auth, url_prefix="/")
+oauth2.register_blueprint(users, url_prefix="/user")
+oauth2.register_blueprint(roles, url_prefix="/role")
+oauth2.register_blueprint(groups, url_prefix="/group")
+oauth2.register_blueprint(resources, url_prefix="/resource")
