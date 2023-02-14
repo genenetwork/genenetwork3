104723d9954359cbc10140c36d4ae4f8d2f4f7f4
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-02-15 00:25:20 +0300

    Create API endpoint for fetching trait metadata
    * gn3/api/metadata.py (dataset_metadata): New function and endpoint.
    (jsonify_dataset_metadata): Rename to dataset_metadata.
    
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- gn3/api/metadata.py
+++ gn3/api/metadata.py
@@ -8,19 +8,35 @@ from flask import current_app
 from SPARQLWrapper import SPARQLWrapper
 
 from gn3.db.rdf import get_dataset_metadata
+from gn3.db.rdf import get_trait_metadata
 
 
 metadata = Blueprint("metadata", __name__)
 
 
 @metadata.route("/dataset/<name>", methods=["GET"])
-def jsonify_dataset_metadata(name):
+def dataset_metadata(name):
     """Fetch a dataset's metadata given it's ACCESSION_ID"""
     try:
         return jsonify(
             get_dataset_metadata(
                 SPARQLWrapper(current_app.config.get("SPARQL_ENDPOINT")),
                 name,
+            ).data
+        )
+    # The virtuoso server is misconfigured or it isn't running at all
+    except (RemoteDisconnected, URLError):
+        return jsonify({})
+
+
+@metadata.route("/dataset/<dataset_name>/trait/<trait_name>", methods=["GET"])
+def trait_metadata(dataset_name, trait_name):
+    """Fetch a trait's metadata given the trait_name and dataset_name'"""
+    try:
+        return jsonify(
+            get_trait_metadata(
+                SPARQLWrapper(current_app.config.get("SPARQL_ENDPOINT")),
+                trait_name, dataset_name
             ).data
         )
     # The virtuoso server is misconfigured or it isn't running at all
