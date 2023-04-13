e55bcc5c90fca5d2d733589d32f8345f15a4f8d9
zsloan <zachary.a.sloan@gmail.com>
2023-04-13 19:09:57 +0000

    Add function for retrieving group name (given group ID)

--- gn3/db/datasets.py
+++ gn3/db/datasets.py
@@ -33,6 +33,27 @@ def retrieve_sample_list(
             samplelist = headers[3:]
         return samplelist
 
+def retrieve_group_name(
+        group_id: int, connection: any):
+    """
+    Given the group id (InbredSet.Id in the database), retrieve its name
+    """
+    query = (
+        "SELECT Name "
+        "FROM InbredSet "
+        "WHERE "
+        "InbredSet.Id = %(group_id)s")
+    with connection.cursor() as cursor:
+        cursor.execute(
+            query,
+            {
+                "group_id": group_id
+            })
+        res = cursor.fetchone()
+        if res:
+            return res[0]
+        return None
+
 def retrieve_probeset_trait_dataset_name(
         threshold: int, name: str, connection: Any):
     """
