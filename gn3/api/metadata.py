718822e2b56ec77b67bc7323520661caf4f2193b
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-06-07 09:37:01 +0300

    Remove trait_metadata endpoint
    * gn3/api/metadata.py: Remove get_trait_metadata import.
    (trait_metadata): Delete.
    
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- gn3/api/metadata.py
+++ gn3/api/metadata.py
@@ -9,7 +9,6 @@ from flask import current_app
 from SPARQLWrapper import SPARQLWrapper
 
 from gn3.db.rdf import get_dataset_metadata
-from gn3.db.rdf import get_trait_metadata
 from gn3.db.rdf import sparql_query
 from gn3.db.rdf import RDF_PREFIXES
 
@@ -32,14 +31,9 @@ def dataset(name):
         return jsonify({})
 
 
-@metadata.route("/dataset/<dataset_name>/trait/<trait_name>", methods=["GET"])
-def trait_metadata(dataset_name, trait_name):
-    """Fetch a trait's metadata given the trait_name and dataset_name'"""
     try:
         return jsonify(
-            get_trait_metadata(
                 SPARQLWrapper(current_app.config.get("SPARQL_ENDPOINT")),
-                trait_name, dataset_name
             ).data
         )
     # The virtuoso server is misconfigured or it isn't running at all
