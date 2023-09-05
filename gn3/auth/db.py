bf8296c5c3093aef32a5cd91fb008ad257e7a2f1
Munyoki Kilyungi <me@bonfacemunyoki.com>
2023-09-05 14:40:35 +0300

    Remove un-necessary ellipsis
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- gn3/auth/db.py
+++ gn3/auth/db.py
@@ -10,40 +10,34 @@ class DbConnection(Protocol):
     """Type annotation for a generic database connection object."""
     def cursor(self) -> Any:
         """A cursor object"""
-        ...
 
     def commit(self) -> Any:
         """Commit the transaction."""
-        ...
 
     def rollback(self) -> Any:
         """Rollback the transaction."""
-        ...
+
 
 class DbCursor(Protocol):
     """Type annotation for a generic database cursor object."""
     def execute(self, *args, **kwargs) -> Any:
         """Execute a single query"""
-        ...
 
     def executemany(self, *args, **kwargs) -> Any:
         """
         Execute parameterized SQL statement sql against all parameter sequences
         or mappings found in the sequence parameters.
         """
-        ...
 
     def fetchone(self, *args, **kwargs):
         """Fetch single result if present, or `None`."""
-        ...
 
     def fetchmany(self, *args, **kwargs):
         """Fetch many results if present or `None`."""
-        ...
 
     def fetchall(self, *args, **kwargs):
         """Fetch all results if present or `None`."""
-        ...
+
 
 @contextlib.contextmanager
 def connection(db_path: str, row_factory: Callable = sqlite3.Row) -> Iterator[DbConnection]:
