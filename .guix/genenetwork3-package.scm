b90aafb139eac3f5208122bf34a462b270ee3e1a
Arun Isaac <arunisaac@systemreboot.net>
2023-12-19 20:32:22 +0000

    guix: Add genenetwork3-all-tests.
    * .guix/genenetwork3-package.scm: Import python-pylint from (gnu packages
    check) and python-mypy from (gnu packages python-check).
    (genenetwork3-all-tests): New variable.

--- .guix/genenetwork3-package.scm
+++ .guix/genenetwork3-package.scm
@@ -1,6 +1,8 @@
 (define-module (genenetwork3-package)
   #:use-module ((gn packages genenetwork)
                 #:select (genenetwork3) #:prefix gn:)
+  #:use-module ((gnu packages check) #:select (python-pylint))
+  #:use-module ((gnu packages python-check) #:select (python-mypy))
   #:use-module (guix gexp)
   #:use-module (guix utils)
   #:use-module (guix git-download)
@@ -15,5 +17,24 @@
                         #:recursive? #t
                         #:select? (or (git-predicate (dirname (current-source-directory)))
                                       (const #t))))))
+
+(define-public genenetwork3-all-tests
+  (package
+    (inherit genenetwork3)
+    (arguments
+     (substitute-keyword-arguments (package-arguments genenetwork3)
+       ((#:phases phases #~%standard-phases)
+        #~(modify-phases #$phases
+            (add-before 'build 'pylint
+              (lambda _
+                (invoke "pylint" "main.py" "setup.py" "wsgi.py" "setup_commands"
+                        "tests" "gn3" "scripts" "sheepdog")))
+            (add-after 'pylint 'mypy
+              (lambda _
+                (invoke "mypy" ".")))))))
+    (native-inputs
+     (modify-inputs (package-native-inputs genenetwork3)
+       (prepend python-pylint)
+       (prepend python-mypy)))))
 
 genenetwork3
