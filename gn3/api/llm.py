1a097b9c5ccadda348c2f2c5eaa65a0b95fabbad
Alexander_Kabui <alexanderkabua@gmail.com>
2024-04-04 02:31:21 +0300

    pep8 formatting and code refactoring

--- gn3/api/llm.py
+++ gn3/api/llm.py
@@ -53,7 +53,8 @@ def gnqa():
         with (require_oauth.acquire("profile") as token, Redis.from_url(current_app.config["REDIS_URI"],
                                                                         decode_responses=True) as redis_conn):
             redis_conn.setex(
-                f"LLM:{str(token.user.user_id)}-{query}", timedelta(days=10), json.dumps(response))
+                f"LLM:{str(token.user.user_id)}-{query}",
+                timedelta(days=10), json.dumps(response))
         return jsonify(response)
     except Exception as error:
         return jsonify({"query": query, "error": f"Request failed-{str(error)}"}), 500
@@ -63,31 +64,30 @@ def gnqa():
 @require_oauth("profile")
 def rating(task_id):
     try:
-        with require_oauth.acquire("profile") as token:
+        with (require_oauth.acquire("profile") as token,
+              db.connection(os.path.join("SQLITE_DB_PATH", "llm.db")) as conn):
             results = request.json
             user_id, query, answer, weight = (str(token.user.user_id),
                                               results.get("query"),
                                               results.get("answer"),
                                               results.get("weight", 0))
-
-            with db.connection(os.path.join(SQLITE_DB_PATH, "llm.db")) as conn:
-                cursor = conn.cursor()
-                create_table = """CREATE TABLE IF NOT EXISTS Rating(
-                      user_id TEXT NOT NULL,
-                      query TEXT NOT NULL,
-                      answer TEXT NOT NULL,
-                      weight INTEGER NOT NULL DEFAULT 0,
-                      task_id TEXT NOT NULL UNIQUE
-                      )"""
-                cursor.execute(create_table)
-                cursor.execute("""INSERT INTO Rating(user_id,query,answer,weight,task_id)
-                VALUES(?,?,?,?,?)
-                ON CONFLICT(task_id) DO UPDATE SET
-                weight=excluded.weight
-                """, (str(user_id), query, answer, weight, task_id))
-                return {
-                    "message": "Thanks for the Feedback"
-                }, 200
+            cursor = conn.cursor()
+            create_table = """CREATE TABLE IF NOT EXISTS Rating(
+                  user_id TEXT NOT NULL,
+                  query TEXT NOT NULL,
+                  answer TEXT NOT NULL,
+                  weight INTEGER NOT NULL DEFAULT 0,
+                  task_id TEXT NOT NULL UNIQUE
+                  )"""
+            cursor.execute(create_table)
+            cursor.execute("""INSERT INTO Rating(user_id,query,answer,weight,task_id)
+            VALUES(?,?,?,?,?)
+            ON CONFLICT(task_id) DO UPDATE SET
+            weight=excluded.weight
+            """, (str(user_id), query, answer, weight, task_id))
+            return {
+                "message": "Thanks for the Feedback"
+            }, 200
     except sqlite3.Error as error:
         raise error
 
@@ -97,7 +97,6 @@ def rating(task_id):
 def fetch_history():
     with (require_oauth.acquire("profile") as token, Redis.from_url(current_app.config["REDIS_URI"],
                                                                     decode_responses=True) as redis_conn):
-
         records = [{result: result.rpartition(
             '-')[-1]} for result in redis_conn.keys(f"LLM:{str(token.user.user_id)}*")]
         return jsonify({
@@ -108,6 +107,6 @@ def fetch_history():
 @GnQNA.route("/history/<query>", methods=["GET"])
 @require_oauth("profile")
 def fetch_user_hist(query):
-    with (require_oauth.acquire("profile") as token, Redis.from_url(current_app.config["REDIS_URI"],
-                                                                    decode_responses=True) as redis_conn):
+    with (Redis.from_url(current_app.config["REDIS_URI"],
+                         decode_responses=True) as redis_conn):
         return jsonify(fetch_query_results(query, redis_conn))
