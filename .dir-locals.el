6811da1c6afe46d27f4b41ff75d8a91e65c0757e
Munyoki Kilyungi <me@bonfacemunyoki.com>
2024-04-15 12:22:41 +0300

    Show trailing white spaces in python-mode.
    Signed-off-by: Munyoki Kilyungi <me@bonfacemunyoki.com>

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,12 @@
 ((python-mode
-  . ((flycheck-python-pylint-executable . "env PYTHONPATH=$HOME/.guix-profile/lib/python3.10/site-packages/ $HOME/.guix-extra-profiles/genenetwork3/bin/pylint")
-     (python-shell-interpreter . "$HOME/.guix-extra-profiles/genenetwork3/bin/python3"))))
+  . ((flycheck-python-pylint-executable . "env PYTHONPATH=$HOME/.guix-extra-profiles/genenetwork3/lib/python3.10/site-packages/ $HOME/.guix-extra-profiles/genenetwork3/bin/pylint")
+     (python-indent . 4)
+     (show-trailing-whitespace . 1)
+     (python-shell-interpreter . "$HOME/.guix-extra-profiles/genenetwork3/bin/python3")
+     ()
+     (eval .
+	   (when
+	       (require 'flycheck nil t')
+	     (setq elpy-modules
+		   (delq 'elpy-module-flymake elpy-modules))
+	     (add-hook 'elpy-mode-hook 'flycheck-mode))))))
