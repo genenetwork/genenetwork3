187a4eee8ab52bcffdf0cd30a3f3252f77ba0d10
Alexander_Kabui <alexanderkabua@gmail.com>
2024-11-20 18:58:58 +0300

    feat: Add initial endpoint for rqtl2 api.

---
+++ gn3/api/rqtl2.py
@@ -0,0 +1,14 @@
+""" File contains the endpoints for calling rqtl2
+"""
+from flask import jsonify
+from flask import Blueprint
+
+rqtl2 = Blueprint("rqtl2", __name__)
+
+
+@rqtl2.route("/compute", method=["GET"])
+def compute():
+    """Init endpoint for computing qtl anaylsis using rqtl2"""
+    # preprocessing data from the client
+    # define required inputs
+    return jsonify({"data": []})
