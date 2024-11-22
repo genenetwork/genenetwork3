656014370380951577900f31e66e9a92c995b0bd
Alexander_Kabui <alexanderkabua@gmail.com>
2024-11-22 12:16:08 +0300

    feat: implement ednpoint for computing qtl using rqtl2. Capture stdout results to a file.

---
+++ gn3/api/rqtl2.py
@@ -0,0 +1,38 @@
+""" File contains endpoints for rqlt2"""
+
+import subprocess
+import uuid
+import os
+from flask import current_app
+from flask import jsonify
+from flask import Blueprint
+
+rqtl2 = Blueprint("rqtl2", __name__)
+
+@rqtl2.route("/compute", methods=["GET"])
+def compute():
+    """Endpoint for computing QTL analysis using R/QTL2"""
+    wkdir = current_app.config.get("TMPDIR")
+    output_file = os.path.join(wkdir, "output.txt")
+    rscript_cmd = (
+        f"Rscript ./scripts/rqtl2_wrapper.R "
+        f"-i /home/kabui/r_playground/meta_grav.json "
+        f"-d /home/kabui/r_playground "
+        f"-o /home/kabui/r_playground/rqtl_output.json "
+        f"--nperm 100 --threshold 1 --cores 0"
+    )
+    process = subprocess.Popen(
+        rscript_cmd, shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    # TODO rethink where we write this file
+    with open(output_file, "a+") as file_handler:
+        for line in iter(process.stdout.readline, b""):
+            file_handler.write(line.decode("utf-8"))
+    process.stdout.close()
+    process.wait()
+    if process.returncode == 0:
+        return jsonify({"msg": "success", "results": "file_here"})
+    else:
+        return jsonify({"msg": "fail", "error": "Process failed"})
