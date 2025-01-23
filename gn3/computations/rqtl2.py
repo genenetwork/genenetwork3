0c42a8d8372933475ec795816dbc13f62ed6b621
Alexander_Kabui <alexanderkabua@gmail.com>
2025-01-23 13:52:18 +0300

    feat: Add function to generate rqtl2 data files.

--- gn3/computations/rqtl2.py
+++ gn3/computations/rqtl2.py
@@ -5,6 +5,25 @@ import json
 from pathlib import Path
 from typing import Dict
 
+def generate_rqtl2_files(data, workspace_dir):
+    """Prepare data  and generate necessary CSV  files
+    required to write to control_file
+    """
+    # Map of file names to corresponding data keys in the provided dictionary
+    file_to_name_map = {
+        "geno_file": "geno_data",
+        "pheno_file": "pheno_data",
+        "geno_map_file": "geno_map_data",
+        "pheno_map_file": "pheno_map_data",
+        "phenocovar_file": "phenocovar_data",
+    }
+    parsed_files = {}
+    for file_name, data_key in file_to_name_map.items():
+        if data_key in data:
+            file_path = write_to_csv(workspace_dir, f"{file_name}.csv", data[data_key])
+            parsed_files[file_name] = file_path
+    return {**data, **parsed_files}
+
 
 def write_to_csv(work_dir, file_name, data:list[dict],
                       headers= None, delimiter=","):
